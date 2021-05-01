da9b88b7d1bfa26b4214b4bc738e1ca3b2454c74
Vinícius Leitão <vinileitao2000@gmail.com>
2021-05-01 19:33:03 -0300

    Nav e header prontos, section sendo criada

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.set('views', __dirname + '/public');
+
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('views/index');
 })
 
 app.listen(4000, () => console.log('Server is running!'));
